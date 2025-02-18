7096a5060c3bfe5b58666df6a6fda5fba3bf952c
Eultype <Sdarryy59@gmail.com>
2025-02-18 13:33:09 +0100

    prepa index.php

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+# Prepa_TI1_C2\public\index.php
+
+/*
+Notre contrôleur frontal
+Il est la seule page publique du site
+*/
+
+// si on a une variable get nommée p
+if(isset($_GET['p'])){
+
+    // création d'un switch qui va vérifier la variable get p
+    switch($_GET['p']){
+        case "entreprise":
+            include "../view/entrepriseView.php";
+            break;
+        case "interview":
+            include "../view/interviewView.php";
+            break;
+        case "expo":
+            include "../view/expositionView.php";
+            break;
+        case "conclusion":
+            include "../view/conclusionView.php";
+            break;
+        case "galerie":
+            include "../view/galerieView.php";
+            break;
+        // variable p pas dans le switch 
+        default:
+            include "../view/error404View.php";
+    }
+
+// sinon nous sommes sur l'accueil    
+}else{
+    // Appel de la vue
+    include "../view/homepageView.php";
+}
